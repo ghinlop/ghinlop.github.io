91348956a71e492ac8d2420ba0816a329819d171
Ghin Lop <ghinlop@gmail.com>
2018-05-02 17:07:48 +0700

    bố trí lại bố cục thư mục
    - Chuyển thư mục dist ra ngoài root
    - Xóa các thư mực dist trong dev và prod
    - Sửa lại code file gulp.js
    - Chỉnh sửa lôi Path đến static file

--- bdsv1/gulpfile.js
+++ bdsv1/gulpfile.js
@@ -7,18 +7,9 @@ const gulp          = require('gulp')
     , st            = require('st')
     , path          = require('path')
     , plumber       = require('gulp-plumber')
-    
-gulp.task('copy', () => {
-    gulp.src([
-        'dev/dist/**/*.*',
-        '!dev/dist/**/*.scss'
-
-    ])
-        .pipe(gulp.dest('./prod/dist'))
-})
 
 gulp.task('sass', () => {
-   gulp.src('./dev/dist/main/scss/*.scss')
+   gulp.src('./dev/scss/*.scss')
         .pipe(plumber({
             errorHandler: function (error) {
                     console.log(error.toString());
@@ -28,7 +19,7 @@ gulp.task('sass', () => {
         )
         .pipe(sass())
         .pipe(minify())
-        .pipe(gulp.dest('./prod/dist/main/css'))
+        .pipe(gulp.dest('../dist/main/css'))
         .pipe(liveReload())
 });
 
@@ -48,7 +39,7 @@ gulp.task('pug', () => {
 gulp.task('watch-sass', () => {
     liveReload.listen();
     gulp.watch([
-        './dev/dist/main/scss/**/*.scss'],
+        './dev/scss/**/*.scss'],
         ['sass']);
 })
 gulp.task('watch-pug', () => {
@@ -62,6 +53,6 @@ gulp.task('server', function(done) {
     http.createServer(
       st({ path: __dirname + '/prod', index: 'index.html', cache: false })
     ).listen(8080, done);
-    gulp.run(['watch-sass', 'watch-pug', 'copy'])
+    gulp.run(['watch-sass', 'watch-pug'])
 });
 
